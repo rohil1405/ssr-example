83786052bf9764c24f71555c1df7df011b67de33
rohil1405 <rohil1405shah@gmail.com>
2025-06-25 09:41:32 +0530

    ssr-counter

--- server/server.js
+++ server/server.js
@@ -1,59 +1,93 @@
+// import express from "express";
+// import fs from "fs";
+// import path from "path";
+// import React from "react";
+// import ReactDOMServer from "react-dom/server";
+// import { StaticRouter } from "react-router-dom/server";
+// import App from "../src/App";
+
+// const PORT = 8000;
+// const app = express();
+
+// const metadata = {
+//   "/about": {
+//     title: "About Us",
+//     description: "This is a about page",
+//   },
+//   "/services": {
+//     title: "Services",
+//     description: "this is a server page",
+//   },
+// };
+
+// app.use(express.static(path.resolve(__dirname, "../build")));
+
+// app.get(["/", "/about", "/services"], (req, res) => {
+//   const indexFile = path.resolve("./build/index.html");
+
+//   fs.readFile(indexFile, "utf-8", (err, data) => {
+//     if (err) {
+//       console.error("Error reading index.html:", err.message);
+//       return res.status(500).send("Internal server error");
+//     }
+
+//     const routeMeta = metadata[req.path] || {
+//       title: "React-App",
+//       description: "Default description for our Website.",
+//     };
+
+//     const appHTML = ReactDOMServer.renderToString(
+//       <StaticRouter location={req.url}>
+//         <App />
+//       </StaticRouter>
+//     );
+
+//     const updatedHTML = data
+//       .replace("<title>React App</title>", `<title>${routeMeta.title}</title>`)
+//       .replace(
+//         '<meta name="description" content="Web site created using create-react-app"/>',
+//         `<meta name="description" content="${routeMeta.description}"/>`
+//       )
+//       .replace('<div id="root"></div>', `<div id="root">${appHTML}</div>`);
+
+//     res.send(updatedHTML);
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+ 
 import express from "express";
 import fs from "fs";
 import path from "path";
+
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import { StaticRouter } from "react-router-dom/server";
+
 import App from "../src/App";
 
 const PORT = 8000;
+
 const app = express();
 
-const metadata = {
-  "/about": {
-    title: "About Us",
-    description: "This is a about page",
-  },
-  "/services": {
-    title: "Services",
-    description: "this is a server page",
-  },
-};
-
-app.use(express.static(path.resolve(__dirname, "../build")));
-
-app.get(["/", "/about", "/services"], (req, res) => {
-  const indexFile = path.resolve("./build/index.html");
-
-  fs.readFile(indexFile, "utf-8", (err, data) => {
+app.use("/", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
-      console.error("Error reading index.html:", err.message);
-      return res.status(500).send("Internal server error");
+      console.log(err);
+      return res.status(500).send("Some error happened");
     }
-
-    const routeMeta = metadata[req.path] || {
-      title: "React-App",
-      description: "Default description for our Website.",
-    };
-
-    const appHTML = ReactDOMServer.renderToString(
-      <StaticRouter location={req.url}>
-        <App />
-      </StaticRouter>
-    );
-
-    const updatedHTML = data
-      .replace("<title>React App</title>", `<title>${routeMeta.title}</title>`)
-      .replace(
-        '<meta name="description" content="Web site created using create-react-app"/>',
-        `<meta name="description" content="${routeMeta.description}"/>`
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
       )
-      .replace('<div id="root"></div>', `<div id="root">${appHTML}</div>`);
-
-    res.send(updatedHTML);
+    );
   });
 });
 
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`App launched on ${PORT}`);
+}); 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+// import { Routes, Route } from "react-router-dom";
 
-function Home() {
-  return <h1>Welcome to the Home Page</h1>;
-}
+// function Home() {
+//   return <h1>Welcome to the Home Page</h1>;
+// }
 
-function About() {
-  return <h1>About Us</h1>;
-}
+// function About() {
+//   return <h1>About Us</h1>;
+// }
 
-function Services() {
-  return <h1>Our Services</h1>;
-}
+// function Services() {
+//   return <h1>Our Services</h1>;
+// }
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-    </Routes>
+    <>
+      <p>{count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/about" element={<About />} />
+    //   <Route path="/services" element={<Services />} />
+    // </Routes>
   );
 }
 
