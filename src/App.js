c05350e58dbee8c4c5860f38cbc26046bdaf4fe6
rohil1405 <rohil1405shah@gmail.com>
2025-06-25 10:53:04 +0530

    ssr-json

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ import Services from "./pages/Services";
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
